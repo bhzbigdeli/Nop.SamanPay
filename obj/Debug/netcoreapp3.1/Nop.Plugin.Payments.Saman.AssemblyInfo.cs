1ee677f79fc79817cdbc1a1208b547042286a047
Behzad <bhz.bigdeli@gmail.com>
2026-05-28 15:51:34 +0330

    ignore file added

--- obj/Debug/netcoreapp3.1/Nop.Plugin.Payments.Saman.AssemblyInfo.cs
+++ obj/Debug/netcoreapp3.1/Nop.Plugin.Payments.Saman.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyCopyrightAttribute("Copyright © 2024")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e69779b15cae012e9aebfa569300c65c853aee59")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nop.Plugin.Payments.Saman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nop.Plugin.Payments.Saman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
